e25f58b1fb2e8352b7ff59aca718e11d9fbe46ea
Lucas <sishsh@163.com>
2019-11-20 17:58:26 +0800

    add note

--- assembly/runtime_api.ts
+++ assembly/runtime_api.ts
@@ -3,24 +3,17 @@ import { util } from "./utils";
 import { Notify } from "./notify";
 export declare namespace env{
 
-     // #############
+    // #############
     // # Registers #
     // #############
     //@ts-ignore
     @external("env", "ontio_timestamp")
     export function ontio_timestamp(): u64;
 
-     // #############
-    // # Registers #
-    // #############
     //@ts-ignore
     @external("env", "ontio_block_height")
     export function ontio_block_height(): u32;
 
-
-     // #############
-    // # Registers #
-    // #############
     //@ts-ignore
     @external("env", "ontio_self_address")
     export function ontio_self_address(dst:u32): void;
@@ -41,150 +34,91 @@ export declare namespace env{
     @external("env", "ontio_entry_address")
     export function ontio_entry_address(dst:u32): void;
 
-
-     // #############
-    // # Registers #
-    // #############
     //@ts-ignore
     @external("env", "ontio_check_witness")
     export function ontio_check_witness(addr:u32): u32;
 
-    // #############
-    // # Registers #
-    // #############
     //@ts-ignore
     @external("env", "ontio_get_input")
     export function ontio_get_input(dst_ptr: u32): void;
 
-   // #############
-   // # Registers #
-   // #############
    //@ts-ignore
    @external("env", "ontio_input_length")
    export function ontio_input_length(): u32;
 
-   // #############
-   // # Registers #
-   // #############
    //@ts-ignore
    @external("env", "ontio_return")
    export function ontio_return(ptr:u32, len:u32):void;
 
 
-   // #############
-   // # Registers #
-   // #############
    //@ts-ignore
    @external("env", "ontio_panic")
    export function ontio_panic(ptr:u32, len:u32):void;
 
 
-   // #############
-   // # Registers #
-   // #############
    //@ts-ignore
    @external("env", "ontio_notify")
    export function ontio_notify(ptr:u32, len:u32):void;
 
 
-   // #############
-   // # Registers #
-   // #############
    //@ts-ignore
    @external("env", "ontio_call_contract")
    export function ontio_call_contract(addr:u32,input_ptr:u32, input_len:u32):i32;
 
-    // #############
-    // # Registers #
-    // #############
     //@ts-ignore
     @external("env", "ontio_call_output_length")
     export function ontio_call_output_length(): u32;
 
 
-    // #############
-    // # Registers #
-    // #############
     //@ts-ignore
     @external("env", "ontio_get_call_output")
     export function ontio_get_call_output(dst_ptr: u32): void;
 
 
-    // #############
-    // # Registers #
-    // #############
     //@ts-ignore
     @external("env", "ontio_current_blockhash")
     export function ontio_current_blockhash(block_hash: u32): u32;
 
 
-    // #############
-    // # Registers #
-    // #############
     //@ts-ignore
     @external("env", "ontio_current_txhash")
     export function ontio_current_txhash(block_hash: u32): u32;
 
 
-    // #############
-    // # Registers #
-    // #############
     //@ts-ignore
     @external("env", "ontio_contract_migrate")
     export function ontio_contract_migrate(code: u32,code_len:u32,vm_type:u32,name_ptr:u32,
         name_len:u32,ver_ptr:u32,ver_len:u32,author_ptr:u32,author_len:u32,
         email_ptr:u32,email_len:u32,desc_ptr:u32,desc_len:u32,new_addr_ptr:u32): i32;
 
-    // #############
-    // # Registers #
-    // #############
     //@ts-ignore
     @external("env", "ontio_storage_read")
     export function ontio_storage_read(key: u32,klen:u32,val:u32,vlen:u32,offset:u32): u32;
 
-    // #############
-    // # Registers #
-    // #############
     //@ts-ignore
     @external("env", "ontio_storage_write")
     export function ontio_storage_write(key: u32,klen:u32,val:u32,vlen:u32): void;
 
 
-    // #############
-    // # Registers #
-    // #############
     //@ts-ignore
     @external("env", "ontio_storage_delete")
     export function ontio_storage_delete(key: u32,klen:u32): void;
 
-
-    // #############
-    // # Registers #
-    // #############
     //@ts-ignore
     @external("env", "ontio_sha256")
     export function ontio_sha256(data: u32,len:u32,val:u32): void;
 
-    // #############
-    // # Registers #
-    // #############
     //@ts-ignore
     @external("env", "ontio_contract_create")
     export function ontio_contract_create(code_ptr: u32,code_len:u32,need_storage:u32,name_ptr:u32,
         name_len:u32,ver_ptr:u32,ver_len:u32,author_ptr:u32,author_len:u32,email_ptr:u32,email_len:u32,
         desc_ptr:u32,desc_len:u32,new_addr_ptr:u32): u32;
 
-    // #############
-    // # Registers #
-    // #############
     //@ts-ignore
     @external("env", "ontio_contract_destroy")
     export function ontio_contract_destroy(): void;
 
 
-    // #############
-    // # Registers #
-    // #############
     //@ts-ignore
     @external("env", "ontio_debug")
     export function ontio_debug(data:u32, len:u32): void;
@@ -217,54 +151,75 @@ export namespace runtime_api {
             return res;
         }
     }
-    
+    /**
+     * get current timestamp
+     */
     export function timestamp():u64 {
         return env.ontio_timestamp();
     }
+     /**
+     * get current block height
+     */
     export function block_height() :u32 {
         return env.ontio_block_height();
     }
-    
+     /**
+     * get current contract address
+     */
     export function address() :Address{
         let res = new Uint8Array(20);
         env.ontio_self_address(res.dataStart as u32);
         return new Address(res);
     }
-    
+    /**
+     * get caller contract address
+     */
     export function caller() :Address {
         let res = new Uint8Array(20);
         env.ontio_caller_address(res.dataStart as u32);
         return new Address(res);
     }
-    
+     /**
+     * get entry contract address
+     */
     export function entry_address():Address {
         let res = new Uint8Array(20);
         env.ontio_entry_address(res.dataStart as u32);
         return new Address(res);
     }
-    
+    /**
+     * get current block hash
+     */
     export function current_block_hash():H256 {
         let res = new Uint8Array(32);
         env.ontio_current_blockhash(res.dataStart as u32);
         return new H256(res);
     }
-    
+    /**
+     * get current tx hash
+     */
     export function current_tx_hash():H256 {
         let res = new Uint8Array(32);
         env.ontio_current_txhash(res.dataStart as u32);
         return new H256(res);
     }
-    
+     /**
+     * sha256
+     */
     export function sha256(data: Uint8Array) :H256 {
         let res = new Uint8Array(32);
         env.ontio_sha256(data.dataStart as u32, data.length, res.dataStart as u32);
         return new H256(res);
     }
-    
+    /**
+     * check witness
+     */
     export function check_witness(addr: Address) :bool {
         return env.ontio_check_witness(addr.value.dataStart as u32) == 1;
     }
-    
+    /**
+     * get the function name and function args
+     */
     export function input(): Uint8Array{
         const len = env.ontio_input_length();
         if (len == 0) {
@@ -275,11 +230,12 @@ export namespace runtime_api {
             return data
         }
     }
-    
+     /**
+     * contract return
+     */
     export function ret(data: Uint8Array) :void {
         env.ontio_return(data.dataStart as u32, data.byteLength as u32);
     }
-    
     
     export function notify(data:Uint8Array):void {
         env.ontio_notify(data.dataStart as u32, data.byteLength as u32);
